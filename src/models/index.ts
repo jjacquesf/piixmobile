398620df1c0f7ee3670bbee63b35236f54163a4d
Jorge Jacques <jjacques@gmail.com>
2023-09-04 22:53:16 -0600

    Export profile model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 export * from './organization.model';
 export * from './product.category.model';
 export * from './product.model';
+export * from './profile.model';
 export * from './profile_role.model';
 export * from './role.model';
 
