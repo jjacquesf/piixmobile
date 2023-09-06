681c9d2807008eac71aeabebc11eed0b548d2ef7
Jorge Jacques <jjacques@gmail.com>
2023-09-06 07:45:24 -0600

    Protect organization services

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,3 +1,4 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
@@ -10,7 +11,7 @@ import {del, get, getModelSchemaRef, param, patch, post, put, requestBody, respo
 import {Organization, OrganizationPayload} from '../models';
 import {OrganizationRepository} from '../repositories';
 
-// @authenticate('jwt')
+@authenticate('jwt')
 export class OrganizationController {
   constructor(
     @repository(OrganizationRepository)
